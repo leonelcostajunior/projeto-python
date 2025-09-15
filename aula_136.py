2da4e25669108121cf86dfeb17df47cbb55dc575
Leonel <leonel.paiva.costa@hotmail.com>
2025-09-14 21:53:45 -0300

    Incrementando com show databases

--- aula_136.py
+++ aula_136.py
@@ -9,4 +9,10 @@ conexao = pymysql.connect(
 
 mycursor = conexao.cursor()
 
-mycursor.execute("CREATE DATABASE mydatabase")
+#mycursor.execute("CREATE DATABASE mydatabase")
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+
+    print(x)
